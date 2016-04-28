162655ca8ea47cb4a0b797a7f132a3b4ce546c00
askashok <askashok@users.noreply.github.com>
2016-04-28 11:24:26 +0530

    Update Testing.java

--- src/pra/Testing.java
+++ src/pra/Testing.java
@@ -26,9 +26,9 @@ public class Testing {
       driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
       //driver.findElement(By.name("Enter your name")).clear();
       driver.findElement(By.name("Enter your name")).sendKeys("ashok");
-      driver.findElement(By.xpath("//android.widget.Button[contains(@bounds,'[192,655][384,853]')]")).click();
-      driver.findElement(By.xpath("//android.widget.Button[contains(@bounds,'[432,655][624,853]')]")).click();
-      driver.findElement(By.xpath("//android.widget.Button[contains(@bounds,'[672,655][864,853]')]")).click();
+      driver.findElement(By.xpath("//android.widget.Button[contains(@bounds,'[192,1087][384,1285]')]")).click();
+      driver.findElement(By.xpath("//android.widget.Button[contains(@bounds,'[432,1087][624,1285]')]")).click();
+      driver.findElement(By.xpath("//android.widget.Button[contains(@bounds,'[672,1087][864,1285]')]")).click();
       //driver.findElement(By.name("Enter your Email")).clear();
       driver.findElement(By.name("Enter your Email")).sendKeys("ashok.blisslogix@gmail.com");
       driver.findElement(By.name("Save")).click();
